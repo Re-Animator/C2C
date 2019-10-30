5e2d281224070fa999b9b1a965303a8c4397b47e
Re-animator <reanimator1904@gmail.com>
2019-10-30 23:21:35 +0300

    30.10.19 Creation

---
+++ app/src/main/java/reanimator1904/gmail/c2c/MainActivity.java
@@ -0,0 +1,13 @@
+package reanimator1904.gmail.c2c;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
